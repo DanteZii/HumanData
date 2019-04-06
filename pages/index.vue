c03bb8728bc19787453822d0b0653598cececb5e
Dante <guge862055200@gmail.com>
2019-04-06 18:57:04 +0200

    init

--- pages/index.vue
+++ pages/index.vue
@@ -2,61 +2,14 @@
 <template>
     <section class="container2">
         <h1 class="h1k">HumanData</h1>
-        <div class="wrapper">
-          <nuxt-link to='/people/chenguanxu' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">陈冠旭</li>
-              </ul>
-          </nuxt-link>    
-          <nuxt-link to='/people/chenhan' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">陈晗</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">郭慧琳</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">蛇皮</li>
-              </ul>
-          </nuxt-link> 
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">姜敬伟</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">王泽华</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">子慕</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/people/chenLiangLiang' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">陈亮亮</li>
-              </ul>
-          </nuxt-link>
-          <nuxt-link to='/' class="alink">
-              <ul class="wriper">
-                  <li class="image"></li>
-                  <li class="name">小田</li>
-              </ul>
-          </nuxt-link>
+        <div class="wrapper" v-for="item in items"
+           :key="item.id">
+            <nuxt-link :to="{path: item.link}"  class="alink">
+            <ul class="wriper">
+                <li class="image">{{item.image}}</li>
+                <li class="name">{{item.name}}</li>
+            </ul>
+           </nuxt-link>
         </div>
         <h1></h1>
         <h1></h1>
@@ -72,15 +25,21 @@ export default {
   
   data() {
     return{
-      todolist: [
-        { name: '陈冠旭',},
-        { name: '陈晗'},
+      items: [
+        { name: '陈冠旭',
+          link: '/people/chenguanxu'
+        },
+        { name: '陈晗',
+          link: '/people/chenhan'
+        },
         { name: '郭慧琳'},
         { name: '蛇皮'},
         { name: '王泽华'},
         { name: '子慕'},
         { name: '姜敬伟'},
-        { name: '亮亮'},
+        { name: '陈欣宇',
+          link: '/people/chenLiangLiang'
+        },
         { name: '小田'},
 
       ]
